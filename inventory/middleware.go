e8d16be3f8e9a29ea4560861476568fa5d3564e4
Sirapat Siribandit <sirapat.siribandit@agoda.com>
2021-06-05 22:27:25 +0700

    Add middleware.

---
+++ inventory/middleware.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	prometheus.Register(totalRequests)
+	prometheus.Register(responseStatus)
+	prometheus.Register(httpDuration)
+}
+
+type StatusRecorder struct {
+	http.ResponseWriter
+	Status int
+}
+
+func (r *StatusRecorder) WriteHeader(status int) {
+	r.Status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
+func NewStatusRecoder(w http.ResponseWriter) *StatusRecorder {
+	return &StatusRecorder{
+		ResponseWriter: w,
+		Status:         http.StatusOK,
+	}
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		recorder := NewStatusRecoder(w)
+
+		next.ServeHTTP(recorder, r)
+		elapsed := time.Since(start)
+
+		log.WithFields(log.Fields{
+			"host":       r.Host,
+			"path":       r.URL.Path,
+			"method":     r.Method,
+			"statuscode": recorder.Status,
+			"elapsed":    elapsed.Milliseconds(),
+		}).Info("Incoming request")
+		fmt.Println("logging2", recorder.Status)
+	})
+}
+
+var totalRequests = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Number of get requests.",
+	},
+	[]string{"path"},
+)
+
+var responseStatus = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "response_status",
+		Help: "Status of HTTP response",
+	},
+	[]string{"status"},
+)
+
+var httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "http_response_time_seconds",
+	Help: "Duration of HTTP requests.",
+}, []string{"path"})
+
+func metricMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+
+		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
+		recorder := NewStatusRecoder(w)
+		next.ServeHTTP(recorder, r)
+
+		statusCode := recorder.Status
+
+		responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
+		totalRequests.WithLabelValues(path).Inc()
+
+		timer.ObserveDuration()
+	})
+}
+
+func recoverMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+
+				log.WithFields(log.Fields{
+					"host":       r.Host,
+					"path":       r.URL.Path,
+					"method":     r.Method,
+					"statuscode": http.StatusInternalServerError,
+					"error":      err,
+				}).Error(err)
+				writeErrorResponse(w, http.StatusInternalServerError)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
