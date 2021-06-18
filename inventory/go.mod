3e83cbd1e2c1938f6593900c0f77a310ce8eb31a
Sirapat Siribandit <sirapat.siribandit@agoda.com>
2021-06-19 00:04:21 +0700

    Add body assertion.

--- inventory/go.mod
+++ inventory/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/prometheus/client_golang v1.10.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.8.0
+	github.com/stretchr/testify v1.7.0
 	github.com/tinylib/msgp v1.1.5 // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/text v0.3.6 // indirect
--- inventory/integrations/component_test.go
+++ inventory/integrations/component_test.go
@@ -4,11 +4,14 @@ package integration
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAddProduct(t *testing.T) {
@@ -35,8 +38,24 @@ func TestAddProduct(t *testing.T) {
 	}
 	defer resp.Body.Close()
 
-	io.Copy(ioutil.Discard, resp.Body)
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("expected status code (%v), got (%v)", http.StatusOK, resp.StatusCode)
 	}
+
+	product := struct {
+		ID     int64  `json:"id"`
+		Name   string `json:"name"`
+		Price  int    `json:"price"`
+		Amount int    `json:"amount"`
+	}{}
+	err = json.NewDecoder(resp.Body).Decode(&product)
+	if err != nil {
+		t.Errorf("got err while reading body: %v", err)
+	}
+
+	assert.Equal(t, product.Name, "iPhone X")
+	assert.Equal(t, product.Price, 2990000)
+	assert.Equal(t, product.Amount, 100)
+
+	io.Copy(ioutil.Discard, resp.Body)
 }
